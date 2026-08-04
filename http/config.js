09891dcf508c2e95c5794f768d048f95b65a8aa7
Inna1306 <inna742@gmail.com>
2026-08-04 18:16:36 +0300

    Update .gitignore

---
+++ http/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // API-ключ
+    WEATHER_API_KEY: process.env.WEATHER_API_KEY || '',
+
+    // Базовый URL для текущей погоды
+    WEATHER_API_URL: 'http://api.openweathermap.org/data/2.5/weather',
+
+
+    UNITS: 'metric',
+
+    LANG: 'ru',
+};
